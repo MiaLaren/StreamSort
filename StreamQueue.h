8b8b322e6b1cf7a940f53a0f17af010c471106f2
Mia <mialaren11@gmail.com>
2026-05-07 02:22:12 +0000

    Added the class declaration for StreamQueue, this includes all member variables and function prototypes

--- StreamQueue.h
+++ StreamQueue.h
@@ -1,1 +1,25 @@
+#ifndef STREAMQUEUE_H
+#define STREAMQUEUE_H
 
+#include <vector>
+#include <string>
+
+class StreamQueue {
+private:
+    std::vector<std::string> titles;
+    std::vector<double> ratings;
+
+public:
+    void addShow(const std::string& title, double rating);
+    void removeShow(int index);
+    void displayAll() const;
+
+    void sortByRating();
+    void searchByTitle(const std::string& title) const;
+
+    // RECURSIVE FUNCTIONS
+    double sumRatings(int index) const;
+    int findHighest(int index) const;
+};
+
+#endif
